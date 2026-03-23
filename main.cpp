947bfc4fea2af4bab4b2fede93a6b804aadc44b5
a1dram <aldram54@gmail.com>
2026-03-23 12:47:15 +0300

    added test2 and test1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include "vector-top-it.hpp"
 
+using topit::Vector;
+
 bool test1() {
-  using topit::Vector;
   Vector< int > v;
   return v.isEmpty();
 }
 
+bool test2() {
+  Vector< int > v;
+  return v.getSize() == 0;
+}
+
+bool test3() {
+  constexpr size_t size = 3ull;
+  Vector< int > v(size);
+  return v.getSize() == size;
+}
+
 int main() {
   using test_f = bool(*)();
-  test_f tests[] = {
-    test1,
+  using case_t = std::pair< test_f, const char* >;
+  case_t tests[] = {
+    {test1, "Default constructed vector must be empty"},
+    {test2, "Default constructed vector size is zero"},
+    {test2, "Vector constructed with size has non-zero size"}
   };
 
-  size_t count = sizeof(tests) / sizeof(test_f);
+  size_t count = sizeof(tests) / sizeof(case_t);
   std::cout << std::boolalpha;
 
   for (size_t i = 0; i < count; ++i) {
-    std::cout << tests[i]() << " : " << i << '\n';
+    std::cout << tests[i].first << " : " << tests[i].second << '\n';
   }
 }
 
--- vector-top-it.hpp
+++ vector-top-it.hpp
@@ -6,12 +6,20 @@ namespace topit {
   struct Vector
   {
     Vector();
+    explicit Vector(size_t s);
     ~Vector();
+
 
     Vector(const Vector< T >&) = delete;
     Vector< T >& operator=(const Vector< T >& ) = delete;
     
+    size_t getSize() const noexcept;
     bool isEmpty() const noexcept;
+
+    T& operator[](size_t id) noexcept;
+    const T& operator[](size_t id) const noexcept;
+    T& at(size_t id);
+    const T& at(size_t id) const;
 
     private:
       T* data_;
@@ -33,8 +41,21 @@ topit::Vector< T >::~Vector() {
 }
 
 template< class T >
+topit::Vector< T >::Vector(size_t size) : 
+  data_(size ? new T[size] : nullptr), 
+  size_(size),
+  capacity_(size) 
+{}
+
+
+template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept {
-  return false;
+  return !size_;
+}
+
+template< class T >
+size_t topit::Vector< T >::getSize() const noexcept {
+  return size_;
 }
 
 #endif
